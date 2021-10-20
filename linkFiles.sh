407170d6c3f47d632785b65e704fa89bc0216324
jimdavis66 <james.norman.davis@gmail.com>
2021-10-20 19:16:14 +1100

    Move to repo folder and symlink files

---
+++ linkFiles.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Symlink dotfiles to the repo
+ln -fs ~/repositories/dotfiles/.macos ~/.macos
+ln -fs ~/repositories/dotfiles/.vimrc ~/.vimrc
+ln -fs ~/repositories/dotfiles/.zprofile ~/.zprofile
+ln -fs ~/repositories/dotfiles/.zshrc ~/.zshrc
